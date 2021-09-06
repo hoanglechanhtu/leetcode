aa62f9b82ca32c1f76b737e1c224c16dafd77800
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-06 22:24:44 +0700

    Update code
    Next greater element without consider circular array

--- NextGreaterElement2.java
+++ NextGreaterElement2.java
@@ -1,5 +1,19 @@
 class Solution {
+    //1 2 1
     public int[] nextGreaterElements(int[] nums) {
-       
+        Deque<Integer> queue = new ArrayDeque<>();
+        int[] res = new int[nums.length];
+        Arrays.fill(res, -1);
+        for (int i = nums.length - 1; i >= 0; i -- ) {
+            int cur = nums[i];
+            while (queue.size() != 0 && queue.getFirst() <= cur) {
+                queue.removeFirst();
+            }
+            if (queue.size() != 0) {
+                res[i] = queue.getFirst();
+            }
+            queue.addFirst(cur);
+        }
+        return res;
     }
 }
