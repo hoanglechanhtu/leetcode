837f120949a315ba271a952db3df555ff3059f73
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-06 22:29:32 +0700

    Update code
    Consider circular array. Duplicate the current array.

--- NextGreaterElement2.java
+++ NextGreaterElement2.java
@@ -2,10 +2,14 @@ class Solution {
     //1 2 1
     public int[] nextGreaterElements(int[] nums) {
         Deque<Integer> queue = new ArrayDeque<>();
-        int[] res = new int[nums.length];
+        int[] newNums = new int[nums.length * 2];
+        for (int i = 0; i < newNums.length; i ++) {
+            newNums[i] = nums[i > nums.length - 1 ? i - nums.length : i];
+        }
+        int[] res = new int[newNums.length];
         Arrays.fill(res, -1);
-        for (int i = nums.length - 1; i >= 0; i -- ) {
-            int cur = nums[i];
+        for (int i = newNums.length - 1; i >= 0; i -- ) {
+            int cur = newNums[i];
             while (queue.size() != 0 && queue.getFirst() <= cur) {
                 queue.removeFirst();
             }
@@ -14,6 +18,6 @@ class Solution {
             }
             queue.addFirst(cur);
         }
-        return res;
+        return Arrays.copyOf(res, nums.length);
     }
 }
