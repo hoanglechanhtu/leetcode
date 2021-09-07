77307fe6ad4d8421041b616fa7509221e5998cca
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-07 20:44:10 +0700

    With O(n) space

--- RotateArray.java
+++ RotateArray.java
@@ -1,5 +1,11 @@
+//1 2 3 4 5 6 || k = 3 -> 4 5 6 1 2 3
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        k = k % n;
+        int[] copyArray = new int[nums.length];
+        Arrays.copy(nums, copyArray);
+        for (int i = 0; i < nums.length; i ++){
+            nums[i] = copyArray[i - k >= 0 ? i - k : nums.length + (i - k) - 1];
+        }
     }
 }
