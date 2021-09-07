cc0765277d9f57132af460af48af41eab07ec2db
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-07 20:45:41 +0700

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,9 +1,8 @@
 //1 2 3 4 5 6 || k = 3 -> 4 5 6 1 2 3
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % n;
-        int[] copyArray = new int[nums.length];
-        Arrays.copy(nums, copyArray);
+        k = k % nums.length;
+        int[] copyArray = Arrays.copyOf/(nums, nums.length);
         for (int i = 0; i < nums.length; i ++){
             nums[i] = copyArray[i - k >= 0 ? i - k : nums.length + (i - k) - 1];
         }
