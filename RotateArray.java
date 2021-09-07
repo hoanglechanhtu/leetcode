26c5c2447c67c740904551ee5e0b025aab1bb10c
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-07 21:26:47 +0700

    Using revert
    Using revert, split the array to two parts.
    Part one is the index i with i + k < nums.length and part two is i + k >= nums.length.
    Reverse both path and then revert whole array.

--- RotateArray.java
+++ RotateArray.java
@@ -1,16 +1,17 @@
 //1 2 3 4 5 6 || k = 3 -> 4 5 6 1 2 3
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
-        for (int i = 0; i < k; i ++) {
-            int tempCur = nums[0];
-            int last = nums[nums.length - 1];
-            for (int j = 1; j < nums.length; j ++) {
-                int temp = nums[j];
-                nums[j] = tempCur;
-                tempCur = temp;
-            }
-            nums[0] = last;
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+    
+    void reverse(int[] nums, int start, int end) {
+        for (int i = 0; i <= (end - start)/2; i ++) {
+            int temp = nums[start + i];
+            nums[start + i] = nums[end - i];
+            nums[end - i] = temp;
         }
+    
     }
 }
