cbbfbd01ae483cd654b7feecec04d6980201971d
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-08 21:13:20 +0700

    Brute force
    Check for every pair (i,j)

--- ContainsDuplicate3.java
+++ ContainsDuplicate3.java
@@ -1,5 +1,11 @@
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        for (int i = 0; i < nums.length; i ++) {
+            for (int j = i + 1; j < nums.length; j ++) {
+                if (j - i >= k && Math.abs(nums[i] - nums[j]) <= t) return true;
+            }
+        }
         
+        return false;
     }
 }
