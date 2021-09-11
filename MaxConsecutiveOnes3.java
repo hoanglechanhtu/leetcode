c04846a9665a58bae69ed36f6ccb99de49c6c46a
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-11 22:45:06 +0700

    Update MaxConsecutiveOnes3.java

--- MaxConsecutiveOnes3.java
+++ MaxConsecutiveOnes3.java
@@ -1,5 +1,7 @@
 //This exercise is similar to https://github.com/hoanglechanhtu/leetcode/blob/main/LongestRepeatingCharacter.java
 //The main difference is, in this excercise, the map only contains 2 values, one and zero, and the left pointer maybe go pass the right pointer, so we have to handle this case
+//HashMap and sliding window
+//Key words: max, consecutives
 
 class Solution {
     public int longestOnes(int[] nums, int k) {
