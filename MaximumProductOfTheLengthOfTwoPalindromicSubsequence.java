fe931cf24c4d08189ed6c2b26418e4a1cd56e000
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-12 15:09:56 +0700

    2002. Maximum Product of the Length of Two Palindromic Subsequences
    Given a string s, find two disjoint palindromic subsequences of s such that the product of their lengths is maximized. The two subsequences are disjoint if they do not both pick a character at the same index.
    
    Return the maximum possible product of the lengths of the two palindromic subsequences.
    
    A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters. A string is palindromic if it reads the same forward and backward.

---
+++ MaximumProductOfTheLengthOfTwoPalindromicSubsequence.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int maxProduct(String s) {
+        if (isSingleChar(s)) return s.length() * s.length()/4;
+        List<List<Integer>> list = new ArrayList<>();
+        build(0, s.toCharArray(), new ArrayList<>(), list);
+        int max = 0;
+        for (int i = 0; i < list.size(); i ++) {
+            for (int j = i + 1; j < list.size(); j ++) {
+                if (isDisjoint(list.get(i), list.get(j))) max = Math.max(max, list.get(i).size() * list.get(j).size());
+            }
+        }
+        
+        return max;
+        
+    }
+    //ab
+    void build(int index, char[] s, List<Integer> cur, List<List<Integer>> list) {
+        if (index == s.length) return;
+        
+        build(index + 1, s, cur, list);
+        cur.add(index);
+        if (isPalindrome(s, cur)) list.add(new ArrayList(cur));
+        build(index + 1, s, cur, list);
+        cur.remove(cur.size() - 1);
+    }
+    
+    boolean isPalindrome(char[] s, List<Integer> cur) {
+        for (int i = 0; i <= cur.size()/2; i ++) {
+            if (s[cur.get(i)] != s[cur.get(cur.size() - i - 1)]) return false;
+        }
+        return true;
+    }
+    
+    boolean isDisjoint(List<Integer> list1, List<Integer> list2) {
+        return list1.stream()
+            .filter(x -> list2.contains(x))
+            .count() == 0;
+    }
+    
+    boolean isSingleChar(String s) {
+        for (int i = 0; i < s.length(); i ++) {
+            if (s.charAt(i) != s.charAt(0)) return false;
+        }
+        return true;
+    }
+}
