549a0ce9da8c300c16871bf9b9a6d434a0245f21
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-14 22:44:29 +0700

    242. Valid Anagram
    Given two strings s and t, return true if t is an anagram of s, and false otherwise.

---
+++ ValidAnagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] charFreq = new int[26];
+        for (int i = 0; i < s.length(); i ++) {
+            charFreq[s.charAt(i) - 'a'] ++;
+            charFreq[t.charAt(i) - 'a'] --;
+        }
+        
+        for (int i = 0; i < 26; i ++) {
+            if (charFreq[i] != 0) return false;
+        }
+        
+        return true;
+    }
+}
