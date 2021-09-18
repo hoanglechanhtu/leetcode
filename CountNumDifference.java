a33575da8ae442551b26b3d158887c94dcc16f51
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-18 23:14:50 +0700

    2006. Count Number of Pairs With Absolute Difference K
    Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
    
    The value of |x| is defined as:
    
    x if x >= 0.
    -x if x < 0.

---
+++ CountNumDifference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int total = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            for (int j = i + 1; j < nums.length; j ++) {
+                if (Math.abs(nums[j] - nums[i]) == k) total ++;
+            }
+        }
+        
+        return total;
+    }
+}
