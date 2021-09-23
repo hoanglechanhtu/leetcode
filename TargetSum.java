83657a73dbb08d31005fbb0901133793130dd3e1
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-23 23:19:40 +0700

    494. Target Sum
    You are given an integer array nums and an integer target.
    
    You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
    
    For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
    Return the number of different expressions that you can build, which evaluates to target.

---
+++ TargetSum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            sum += nums[i];
+        }
+        if (target > sum || target < -sum) return 0;
+        if ((target + sum) % 2 != 0) return 0;
+        int N = (target + sum)/2;
+        int[][] dp = new int[nums.length + 1][N + 1];
+        
+        for (int i = 1; i < dp.length; i ++) {
+            for (int j = 1; j < dp[0].length; j ++) {
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i - 1]] + nums[i - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        int total = 0;
+        for (int i = 0; i < dp.length; i ++) {
+            if (dp[i][N] == N) total += count(dp, i, N, nums);
+        }
+        return total;
+    }
+    
+    int count(int[][] dp, int i, int j, int[] nums) {
+        if (i == 0) return 1;
+        int total = 0;
+        int target = j - nums[i - 1];
+        if (target < 0) return 0;
+        for (int k = 0; k < i; k ++) {
+            if (dp[k][target] == target) total += count(dp, k, target, nums);
+        }
+        
+        return total;
+    }
+}
