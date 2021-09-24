44cebce6e5988b4dd73c79ddf8ac5d3e037a6059
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-09-24 22:51:42 +0700

    Update

--- CoingChange2.java
+++ CoingChange2.java
@@ -73,3 +73,22 @@ class Solution {
         return dp[amount];
     }
 }
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        Arrays.fill(dp[0], 0);
+        dp[0][0] = 1;
+        for (int i = 1; i <= coins.length; i ++) {
+            for (int j = 0; j <= amount; j ++) {
+                if (j >= coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[coins.length][amount];
+    }
+}
