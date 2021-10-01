1b7ae678865443c492b50ffafad3caaab4b74b58
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-10-01 08:40:07 +0700

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -1,6 +1,8 @@
 //1 2 3 4 5 6 || k = 3 -> 4 5 6 1 2 3
 class Solution {
     public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        if (nums.length <= 1 || k == 0) return;
         reverse(nums, 0, nums.length - k - 1);
         reverse(nums, nums.length - k, nums.length - 1);
         reverse(nums, 0, nums.length - 1);
