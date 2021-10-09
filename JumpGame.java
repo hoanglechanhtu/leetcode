366e1ef877ec6970077c0627165b3c91ebdc114b
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-10-09 22:56:49 +0700

    Update JumpGame.java

--- JumpGame.java
+++ JumpGame.java
@@ -9,3 +9,23 @@ class Solution {
         return lastIndex == 0;
     }
 }
+
+class Solution {
+    
+    public boolean canJump(int[] nums) {
+        boolean dp[] = new boolean[nums.length];
+        Arrays.fill(dp, false);
+        dp[0] = true;
+        for (int i = 0; i < nums.length; i ++) {
+            if (!dp[i]) break;
+            for (int k = nums[i]; k > 0; k--) {
+                if (i + k < nums.length) {
+                    if (dp[i + k]) continue;
+                    else dp[i + k] = true;
+                }
+            }
+        }
+        
+        return dp[nums.length - 1];
+    }
+}
