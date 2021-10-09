218864907c9cd29726feb0101eb375009fa562d3
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-10-09 22:39:15 +0700

    54. Spiral Matrix
    Given an m x n matrix, return all elements of the matrix in spiral order.
    
     
    
    Example 1:
    
    
    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
    Output: [1,2,3,6,9,8,7,4,5]
    Example 2:
    
    
    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
    Output: [1,2,3,4,8,12,11,10,9,5,6,7]
     
    
    Constraints:
    
    m == matrix.length
    n == matrix[i].length
    1 <= m, n <= 10
    -100 <= matrix[i][j] <= 100

---
+++ SpiralMatrix.java
@@ -0,0 +1,79 @@
+
+    class Solution {
+        enum Move {
+            RIGHT(0, 1),
+            DOWN(1, 0),
+            LEFT(0, -1),
+            UP(-1, 0),
+            NONE(0,0);
+            int x;
+            int y;
+            Move(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        class Position {
+            public Position(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+
+            public int x;
+            public int y;
+        }
+
+        public List<Integer> spiralOrder(int[][] matrix) {
+            if (matrix.length == 0) return Collections.emptyList();
+            List<Integer> res = new ArrayList();
+            Move curMove = Move.RIGHT;
+            Move nextMove = Move.RIGHT;
+            Position pos = new Position(0, 0);
+            Position min = new Position(0, 0);
+            Position max = new Position(matrix.length - 1, matrix[0].length - 1);
+
+            res.add(matrix[pos.x][pos.y]);
+            while (nextMove != Move.NONE) {
+                nextMove = findNextMove(pos, min, max, curMove);
+                move(pos, nextMove);
+                adjustBound(min, max, curMove, nextMove);
+                curMove = nextMove;
+                if (nextMove != Move.NONE)
+                    res.add(matrix[pos.x][pos.y]);
+            }
+
+
+            return res;
+        }
+
+        public Move findNextMove(Position pos, Position min, Position max, Move currMove) {
+            if (currMove == Move.RIGHT && pos.y < max.y) return Move.RIGHT;
+            if (currMove == Move.RIGHT && pos.x < max.x) return Move.DOWN;
+            if (currMove == Move.DOWN && pos.x < max.x) return Move.DOWN;
+            if (currMove == Move.DOWN && pos.y > min.y) return Move.LEFT;
+            if (currMove == Move.LEFT && pos.y > min.y) return Move.LEFT;
+            if (currMove == Move.LEFT && pos.x > min.x) return Move.UP;
+            if (currMove == Move.UP && pos.x > min.x) return Move.UP;
+            if (currMove == Move.UP && pos.y < max.y) return Move.RIGHT;
+            return Move.NONE;
+        }
+
+
+        public void move(Position pos, Move move) {
+            pos.x += move.x;
+            pos.y += move.y;
+        }
+
+        public void adjustBound(Position min, Position max, Move curMove, Move nextMove) {
+            if (curMove == Move.RIGHT && nextMove == Move.DOWN) {
+                min.x += 1;
+            } else if (curMove == Move.DOWN && nextMove == Move.LEFT) {
+                max.y -= 1;
+            } else if (curMove == Move.LEFT && nextMove == Move.UP) {
+                max.x -= 1;
+            } else if (curMove == Move.UP && nextMove == Move.RIGHT) {
+                min.y += 1;
+            }
+        }
+    }
