c200593cff854ee0bf6c227b42411c0284607d85
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-10-27 15:32:14 +0700

    206. Reverse Linked List
    Given the head of a singly linked list, reverse the list, and return the reversed list.
    
     
    
    Example 1:
    
    
    Input: head = [1,2,3,4,5]
    Output: [5,4,3,2,1]
    Example 2:
    
    
    Input: head = [1,2]
    Output: [2,1]
    Example 3:
    
    Input: head = []
    Output: []
     
    
    Constraints:
    
    The number of nodes in the list is the range [0, 5000].
    -5000 <= Node.val <= 5000
     
    
    Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?

---
+++ ReverseLinkedList.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode pre = new ListNode(0, head);
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        head.next = null;
+        return pre;
+    }
+}
