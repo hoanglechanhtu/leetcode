579bb6563ef205a841abc0737d16d523c9a18fd4
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2021-10-29 23:32:28 +0700

    236. Lowest Common Ancestor of a Binary Tree
    Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
    
    According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
    
     
    
    Example 1:
    
    
    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
    Output: 3
    Explanation: The LCA of nodes 5 and 1 is 3.
    Example 2:
    
    
    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
    Output: 5
    Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
    Example 3:
    
    Input: root = [1,2], p = 1, q = 2
    Output: 1
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [2, 105].
    -109 <= Node.val <= 109
    All Node.val are unique.
    p != q
    p and q will exist in the tree.

---
+++ LowestCommonAncestorNormal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode res;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        find(root, p, q);
+        return res;
+    }
+    
+     
+    boolean find(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+       
+        boolean left = find(root.left, p, q);
+        boolean right = find(root.right, p, q);
+        boolean cur = root.val == q.val || root.val == p.val;
+        if (res == null){
+            if ((left && right) || (left && cur) || (right && cur)) res = root;
+        }
+        
+        return left || right || cur;
+    }
+}
