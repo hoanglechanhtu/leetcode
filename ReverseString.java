f19ac9fa0d48152f4cb7ee65539510794b0d568a
Hoàng Lê Chánh Tú <hoanglechanhtu01@gmail.com>
2022-05-30 21:53:49 +0700

    Add recursion version

--- ReverseString.java
+++ ReverseString.java
@@ -10,3 +10,18 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public void reverseString(char[] s) {
+        reverse(s, 0);
+    }
+    
+    void reverse(char[] s, int i) {
+        if (i < s.length/2) {
+            char temp = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = temp;
+            reverse(s, i + 1);
+        }
+    }
+}
